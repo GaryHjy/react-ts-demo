4f42bad6bd087e2f46d9e37acd2da0578f641fb0
GaryHjy <782316959@qq.com>
2020-04-23 23:47:12 +0800

    feat: 修改actions文件

--- src/store/actions/profile.tsx
+++ src/store/actions/profile.tsx
@@ -1,14 +1,3 @@
-import { TypeAction } from "../../typings/common";
-
-export interface TypeProfile {
-}
-
-let initialState = {
-}
-
-export default function (state: TypeProfile = initialState, action: TypeAction) {
-  switch(action.type) {
-    default:
-      return state;
-  }
+export default {
+  
 }
