f5cc4c4744ae03cae026fdbf5e8fb6aace7b02c5
GaryHjy <782316959@qq.com>
2020-04-26 23:36:11 +0800

    feat: 添加LessonSchema

--- server/config/db.js
+++ server/config/db.js
@@ -20,7 +20,20 @@ const SliderSchema = new mongoose.Schema({
 
 const Slider = conn.model('Slider', SliderSchema);
 
+const LessonSchema = new mongoose.Schema({
+  order: Number,//顺序
+  title: String,//标题
+  video: String,//视频
+  poster: String, //海报
+  url: String,//url地址
+  price: Number,//价格
+  category: String,//分类
+})
+
+const Lesson = conn.model('Lesson', LessonSchema);
+
 module.exports = {
   User,
-  Slider
+  Slider,
+  Lesson
 }
